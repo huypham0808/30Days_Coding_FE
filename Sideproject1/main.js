4372cd3ef48252c47398d6c2090634de5d4ca210
huypham0808 <phamngohoanghuy95@gmail.com>
2025-05-18 12:21:32 +0700

    validate form basic

--- Sideproject1/main.js
+++ Sideproject1/main.js
@@ -3,7 +3,7 @@ const userName = document.getElementById('username');
 const email = document.getElementById('email');
 const passWord = document.getElementById('password');
 const passWord2 = document.getElementById('password2');
-
+//show error message
 const showError = (input, message) => {
     const formControl = input.parentElement;
     formControl.className = "form-control error";
@@ -15,10 +15,16 @@ const showSuccess = (input) => {
     const formControl = input.parentElement;
     formControl.className = "form-control success";
 };
-const isValidEmail = (email) => {
+//check email valid
+const checkEmail = (input) => {
     const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, "Email is required");
+    }
 }
+//check string empty
 const checkRequire = (inputArray) => {
     inputArray.forEach((input) => {
         if (input.value.trim() === "") {
@@ -32,13 +38,25 @@ const checkRequire = (inputArray) => {
 const getFieldName = (input) => {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
-const checkLengthPassword = (pw) => {
-    if (pw.length > 6) {
-        return true;
+const checkLength = (input, min, max) => {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} character`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} character`);
     }
-    return false;
+}
+const checkPasswordMatch = (input1, input2) => {
+    if (input1.value !== input2.value) {
+        showError(input2, "Password not match");
+    } else {
+        showSuccess(input2);
+    }
 }
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     checkRequire([userName, email, passWord, passWord2]);
+    checkEmail(email);
+    checkLength(userName, 3, 15);
+    checkLength(passWord, 6, 25);
+    checkPasswordMatch(passWord, passWord2);
 });
