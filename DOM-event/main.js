bd0963abe0cd0949dc5c69c1c937feffba362164
huypham0808 <phamngohoanghuy95@gmail.com>
2025-07-16 21:54:43 +0700

    review core javascript

--- DOM-event/main.js
+++ DOM-event/main.js
@@ -15,3 +15,37 @@ h2Element.onclick = (e) => {
 for (let i = 0; i < h2Element.length; i++) {
   console.log(h2Element[i]);
 }
+
+function xinChao(userName = "No name") {
+  console.log("Xin chao ban " + userName);
+}
+
+xinChao();
+xinChao("Huy Pham");
+
+let plus = (a, b) => {
+  return a + b;
+};
+
+let totalNum = plus(2, 3);
+console.log("total is " + totalNum);
+
+//Object in Javascript
+
+let person = (ten, tuoi) => ({ "Ten: ": ten, "Tuoi: ": tuoi });
+
+console.log(person("Bi Bi", 20));
+
+class User {
+  constructor(name, age) {
+    (this.ten = name), (this.tuoi = age);
+  }
+  xinChao() {
+    console.log("=>>Xin chao " + this.ten);
+  }
+}
+
+const user1 = new User("Bi Bo", 23);
+console.log("Ten user: " + user1.ten);
+console.log("Tuoi user: " + user1.tuoi);
+user1.xinChao();
